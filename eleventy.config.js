5f1e53137ab08041f5bf1cb2de8e8191e6e94a1c
Claudio <claudio@ccmdesign.ca>
2024-12-09 11:50:06 -0800

    updates feed width and layout

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css/*.*");
   eleventyConfig.addPassthroughCopy("./src/js/*.js");
+  eleventyConfig.addPassthroughCopy("./src/images/*.*");
   eleventyConfig.addWatchTarget("./src/**/*.njk");
   eleventyConfig.addWatchTarget("./src/**/js/*.js");
   eleventyConfig.addWatchTarget("./src/css/*.css");
